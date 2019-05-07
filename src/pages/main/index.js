835f1627e2e9fdc2339bcdb68697882feccdf46e
Gustavo Henrique Michels <ghmichels@gmail.com>
2019-05-07 13:12:17 -0300

    First Commit

---
+++ src/pages/main/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Map from '../../components/Map';
+import LeftBar from '../../components/LeftBar';
+import AddUser from '../../components/AddUser';
+
+const Main = () => (
+  <>
+    <Map />
+    <LeftBar />
+    <AddUser />
+  </>
+);
+
+export default Main;
